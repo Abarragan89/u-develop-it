f40fc3d24edd31f530ae1a163f2bafd4d826a9db
Anthony Barragan <anthony.bar.89@gmail.com>
2022-03-06 15:56:23 -0800

    connected database with express server

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const { urlencoded } = require('body-parser');
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3000
 const app = express();
 // middleware to parse JSON
 app.use(urlencoded({ extended: false }));
 app.use(express.json());
-
-
+// connection to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'anthony',
+        password: 'root',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+// This is the way you make queries from Node
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // catch all for any bad requests
 app.use((req, res) => {
