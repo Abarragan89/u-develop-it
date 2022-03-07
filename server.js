5a7821cf1aae626c4f9bd57fd3dfa5f4c2f8484b
Anthony Barragan <anthony.bar.89@gmail.com>
2022-03-06 16:32:18 -0800

    added database queries to sever.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { urlencoded } = require('body-parser');
 const express = require('express');
 const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 const app = express();
 // middleware to parse JSON
 app.use(urlencoded({ extended: false }));
@@ -17,10 +17,38 @@ const db = mysql.createConnection(
     },
     console.log('Connected to the election database.')
 );
-// This is the way you make queries from Node
+// Select all candidates and candidate information
 db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
 });
+
+// Get a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 7`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+// Delete a candidate
+    // example of a prepared statement (i.e. variable)
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
+// create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
+
 
 // catch all for any bad requests
 app.use((req, res) => {
